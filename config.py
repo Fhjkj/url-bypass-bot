bb403dfda2ea35d5e558ad6a1db6ffc8a6ae6dfa
Fhjkj <120001166+Fhjkj@users.noreply.github.com>
2026-06-27 12:11:22 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+def must_get(name):
+    value = os.getenv(name)
+    if not value:
+        raise ValueError(f"Missing env var: {name}")
+    return value
 
-OWNER_ID = int(os.getenv("OWNER_ID"))
+API_ID = int(must_get("API_ID"))
+API_HASH = must_get("API_HASH")
+BOT_TOKEN = must_get("BOT_TOKEN")
 
-FORCE_JOIN = os.getenv("FORCE_JOIN", "")
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
