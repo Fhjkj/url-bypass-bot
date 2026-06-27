61da9601ddb0f258d5bb067511527f4700553210
Fhjkj <120001166+Fhjkj@users.noreply.github.com>
2026-06-27 10:20:01 +0400

    config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os
 
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+FORCE_JOIN = os.getenv("FORCE_JOIN", "")
