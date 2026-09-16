1e9817b19e996709cf2b4cdde86d3acc6a4835e5
Fhjkj <1.20001166e+08+Fhjkj@users.noreply.github.com>
2026-09-16 08:57:04 +0000

    Show bypass command in scrape card

--- FZBypass/plugins/bypass.py
+++ FZBypass/plugins/bypass.py
@@ -152,7 +152,7 @@ async def bypass_check(client, message):
     for source, bypassed, card_kind in parse_data:
         if card_kind:
             cards.append(
-                f"<blockquote>B <a href=\"{source}\">{source}</a></blockquote>\n"
+                f"<blockquote>/bypass <a href=\"{source}\">{source}</a></blockquote>\n"
                 f"<blockquote>{bypassed}</blockquote>\n\n"
                 "<blockquote>━━━━━━━✦✗✦━━━━━━━</blockquote>\n\n"
                 "<blockquote><b>Powered By <a href=\"https://t.me/Bypass0_bot\">@Bypass0_bot</a></b> ❞</blockquote>"
