7c7e454418db94f51aac1781b7dc9a3d09d1e78f
Fhjkj <1.20001166e+08+Fhjkj@users.noreply.github.com>
2026-09-19 09:15:46 +0000

    Route BabyLinks through generic resolver

--- FZBypass/core/bypass_checker.py
+++ FZBypass/core/bypass_checker.py
@@ -423,6 +423,8 @@ async def direct_link_checker(link, onlylink=False):
         blink = await extract_final_destination(link)
     elif bool(match(r"https?:\/\/(?:www\.)?vplink\.in\/\S+", link)):
         blink = await extract_final_destination(link)
+    elif bool(match(r"https?:\/\/(?:www\.)?babylinks\.in\/\S+", link)):
+        blink = await extract_final_destination(link)
 
     # DL Sites
     elif bool(match(r"https?:\/\/cinevood\.\S+", link)):
